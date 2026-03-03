bf60383c87b991070e13b5e451637c4fdc592759
ymstar <18710551637@163.com>
2026-03-03 19:32:56 +0800

    feat: 添加 Vercel Analytics 集成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inspector } from 'react-dev-inspector';
+import { Analytics } from "@vercel/analytics/next"
 import './globals.css';
 
 export const metadata: Metadata = {
